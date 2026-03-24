7dce8ffac1777d7a7e12e4c18a979d37f87e1356
snowmerak <snowmerak@outlook.com>
2026-03-25 01:18:47 +0900

    feat(example): add testlib as a dependency and integrate its functions

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,11 +1,12 @@
 #include <fmt/core.h>
 #include <nlohmann/json.hpp>
+#include <testlib.h>
 
 int main() {
     nlohmann::json config = {
         {"name", "example"},
         {"version", 1},
-        {"features", {"fmt", "json"}}
+        {"features", {"fmt", "json", "testlib"}}
     };
 
     fmt::print("Config:\n{}\n", config.dump(2));
@@ -13,6 +14,9 @@ int main() {
     for (const auto& feature : config["features"]) {
         fmt::print("  - {}\n", feature.get<std::string>());
     }
+
+    fmt::print("\ntestlib::add(3, 4) = {}\n", testlib::add(3, 4));
+    fmt::print("testlib::multiply(3, 4) = {}\n", testlib::multiply(3, 4));
 
     return 0;
 }
